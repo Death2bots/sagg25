389bbfcb9e914bb96d045704bd11d8c4e913f240
Death2bots <death2bots@yahoo.com>
2014-08-22 19:38:47 -0500

    Files

---
+++ src/sagg25/level/tile/GrassTile.java
@@ -0,0 +1,16 @@
+package sagg25.level.tile;
+
+import sagg25.Screen;
+import sagg25.Graphics.Sprite;
+
+public class GrassTile extends Tile {
+
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		// Render stuff here
+	}
+	
+}
--- src/sagg25/level/tile/Tile.java
+++ src/sagg25/level/tile/Tile.java
@@ -1,5 +1,24 @@
 package sagg25.level.tile;
 
-public class Tile {
+import sagg25.Screen;
+import sagg25.Graphics.Sprite;
 
+public class Tile {
+	
+	public int x, y;
+	public Sprite sprite;
+	
+	
+	
+	public Tile(Sprite sprite) {
+		this.sprite = sprite;
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		}
+	
+	public boolean solid() {
+		return false;
+	}
+	
 }
